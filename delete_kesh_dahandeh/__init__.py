f6b8a05a351606278179ea8d9cab94dbe4d269f8
Rooholah <roohi.abol@gmail.com>
2016-08-03 16:44:25 +0430

    A plugin to delete "Arabic Tatweel" characters in a word.

---
+++ delete_kesh_dahandeh/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__ = 'GPL v3'
+__copyright__ = '2016, taaghche.ir'
+
+from calibre.customize import EditBookToolPlugin
+
+
+class DemoPlugin(EditBookToolPlugin):
+
+    name = 'Delete Kesh_dahandeh'
+    version = (1, 0, 0)
+    author = 'Rooholah Abolhasani'
+    supported_platforms = ['windows', 'osx', 'linux']
+    description = 'Some modifications in epub file'
+    minimum_calibre_version = (1, 46, 0)
